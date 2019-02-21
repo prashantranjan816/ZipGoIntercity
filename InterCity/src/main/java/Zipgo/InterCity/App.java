a55f4aaca598638d04abe0fdabe48ff8c656359f
PrashnatRanjane <prashant.ranjan@zipgo.in>
2019-02-21 11:45:55 +0530

    Fiest Commit.

---
+++ InterCity/src/main/java/Zipgo/InterCity/App.java
@@ -0,0 +1,13 @@
+package Zipgo.InterCity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
